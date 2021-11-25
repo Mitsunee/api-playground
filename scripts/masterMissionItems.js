800b184bfdca89d242f7536774f1992b5e121708
Mitsunee <michelle@mitsunee.com>
2021-11-25 14:04:02 +0100

    update master mission item script to not require a mission id

--- scripts/masterMissionItems.js
+++ scripts/masterMissionItems.js
@@ -1,32 +1,32 @@
 const niceItem = require("../cache/atlas_na/nice_item");
 const niceMM = require("../cache/atlas_na/nice_master_mission");
 
-/*** EDIT THIS LINE ***/
-masterMissionId = 200077;
+for (let masterMission of niceMM) {
+  if (masterMission.id === 10001) continue;
 
-const masterMission = niceMM.find(({ id }) => id === masterMissionId);
-
-if (!masterMission) {
-  console.log("could not find mission ID " + masterMissionId);
-  process.exit(1);
-}
-
-const itemMissions = masterMission.missions.filter(({ conds }) =>
-  conds.some(
-    ({ condType, detail }) =>
-      condType === "missionConditionDetail" && detail.missionCondType === 12
-  )
-);
-itemMissions.forEach(mission => {
-  console.log(mission.detail.substring(mission.detail.indexOf("[-]") + 3));
-  mission.conds
-    .find(
+  // find missions with item conds
+  const itemMissions = masterMission.missions.filter(({ conds }) =>
+    conds.some(
       ({ condType, detail }) =>
         condType === "missionConditionDetail" && detail.missionCondType === 12
     )
-    .detail.targetIds.forEach(id => {
-      const item = niceItem.find(item => item.id === id);
-      console.log(`  - ${item ? item.name : `[ID:${id}]`}`);
-    });
-  console.log("");
-});
+  );
+
+  // render info
+  for (mission of itemMissions) {
+    // display mission detail
+    const colorEnd = mission.detail.indexOf("[-]");
+    console.log(mission.detail.substring(colorEnd > 0 ? colorEnd + 3 : 0));
+
+    // display list
+    mission.conds
+      .find(
+        ({ condType, detail }) =>
+          condType === "missionConditionDetail" && detail.missionCondType === 12
+      )
+      .detail.targetIds.forEach(id => {
+        const item = niceItem.find(item => item.id === id);
+        console.log(`  - ${item ? item.name : `[ID:${id}]`}`);
+      });
+  }
+}
