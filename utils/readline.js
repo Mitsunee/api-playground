984630d95b9d28624bbebad72896831b71ffef50
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2021-12-14 21:43:07 +0100

    add: menu method for readline util

--- utils/readline.js
+++ utils/readline.js
@@ -11,7 +11,37 @@ export function readline() {
       rl.question(`${prompt}: `, input => resolve(input.trim()));
     });
   };
+
+  const menu = async (options, text = "Choose Option") => {
+    function getValue(item) {
+      if (typeof item !== "object") return item;
+      if (item.value === undefined) return item;
+      return item.value;
+    }
+
+    function getLabel(item) {
+      if (typeof item !== "object") return item;
+      if (item.label === undefined) return item;
+      return `${item.label}`;
+    }
+
+    if (options.length === 0) return;
+    if (options.length < 2) {
+      return getValue(options[0]);
+    }
+
+    const list = options.map((option, idx) => `  ${idx}) ${getLabel(option)}`);
+    const selection = parseInt(
+      await question(
+        `\n${text}:\n${list.join("\n")}\nOption (0-${list.length - 1})`
+      )
+    );
+
+    if (isNaN(selection) || selection >= list.length) return null;
+    return getValue(options[selection]);
+  };
+
   const close = () => rl.close();
 
-  return { question, close };
+  return { question, menu, close };
 }
