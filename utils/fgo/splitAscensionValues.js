0171f3a47c030abe6b0234e9b6c21cc0243ae11a
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-01-17 13:29:17 +0100

    change makeAscensionUsage accept arbitrary delimiters as well

--- utils/fgo/splitAscensionValues.js
+++ utils/fgo/splitAscensionValues.js
@@ -1,8 +1,8 @@
 export function splitAscensionValues(input, defaultValue) {
-  const test = /[1-4]\/[1-4]/;
+  const test = /[1-4][^1-4][1-4]/;
   let value = defaultValue;
 
   if (input !== "" && test.test(input)) value = input;
 
-  return value.split("/").map(val => Number(val));
+  return value.split(/[^1-4]/).map(val => Number(val));
 }
