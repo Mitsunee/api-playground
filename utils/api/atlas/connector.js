7075f1ff64e68f7c653d2f4ea3ccd0e4da44e921
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-05-12 14:18:13 +0200

    fix: always uppercase game region in atlas api connector

--- utils/api/atlas/connector.js
+++ utils/api/atlas/connector.js
@@ -5,8 +5,8 @@ import { getApiInfo } from "./getApiInfo.js";
 import { makeApiCache } from "../makeApiCache.js";
 import { attachApiCache } from "../attachApiCache.js";
 
-export async function atlasConnector(regionArg) {
-  const region = regionArg;
+export async function atlasConnector(region) {
+  region = region.toUpperCase();
   const name = `Atlas ${region}`;
   const current = await getApiInfo(region);
   const baseUrl = "https://api.atlasacademy.io";
